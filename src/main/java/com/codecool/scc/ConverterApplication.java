8ed713b4fbe751f8aca60081f1acb6b6e4741dc6
CyprianS <szwajka.cyprian@gmail.com>
2018-01-03 11:48:08 +0100

    Create conditional statement in main, create OutputType enum

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -1,7 +1,18 @@
 package com.codecool.scc;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ConverterApplication {
     public static void main(String[] args) {
-        
+
+        if (args.length == 0) {
+            System.out.println("No input file defined");
+        } else if (args.length == 1) {
+            Path filePath = Paths.get(args[0]);
+        } else if (args.length == 2) {
+
+            Path filePath = Paths.get(args[1]);
+        }
     }
 }
---
+++ src/main/java/com/codecool/scc/OutputType.java
@@ -0,0 +1,4 @@
+package com.codecool.scc;
+
+public enum OutputType {
+}
